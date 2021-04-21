c0cded7242ec9cfa28ce0d10e92d454dfbbadc36
Juhee <juhee.baek@fitogether.com>
2021-04-21 20:57:07 +0900

    [fix] 기본 정보 불러올 때 에러 없애기

--- src/views/selected/LevelList.ts
+++ src/views/selected/LevelList.ts
@@ -1,4 +1,7 @@
-import { Vue } from "vue-class-component";
+import { defineComponent } from "vue";
+import info from "@/assets/data/info.json";
+import { Crumbs } from "@/types/Crumbs";
+import { Games } from "@/types/Games";
 
 interface Level {
   idfLevel: number;
@@ -6,10 +9,41 @@ interface Level {
   value: string;
 }
 
-export default class LevelList extends Vue {
-  level = 12;
+interface GameInfo {
+  idfGame: number;
+  name: string;
+  value: string;
+  description: string;
+}
 
+export default defineComponent({
+  data() {
+    return {
+      idfGame: "",
+      value: "",
+      breadcrumbs: new Array<Crumbs>(),
+      level: 12
+      // gameInfo: {[idfGame: String]: Any}
+    };
+  },
+
+  created(): void {
+    console.log(this.$route.params);
+    const { idfGame, value } = this.$route.params;
+    this.idfGame = idfGame.toString();
+    this.value = value.toString();
+    // type Key = { idfGame: string | number };
+    // const gameInfo = info.games[this.idfGame : Key];
+    console.log(info.games[1]);
+    // this.breadcrumbs = [
+    //   {
+    //     name: "동물",
+    //     to: "/game/list",
+    //     isActive: true
+    //   }
+    // ];
+  },
   mounted() {
     console.log(this.level);
   }
-}
+});
--- src/views/selected/LevelList.vue
+++ src/views/selected/LevelList.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="level__list">
-    <level-button-item :levels="level"></level-button-item>
+    <div class="main__header">
+      <layout-bread-crumbs :item="breadCrumbs"></layout-bread-crumbs>
+    </div>
+    <div class="main__content">
+      <level-button-item
+        :idfGame="idfGame"
+        :themeValue="value"
+        :levels="level"
+      ></level-button-item>
+    </div>
   </div>
 </template>
 
 <script lang="ts" src="./LevelList.ts"></script>
 
 <style scoped lang="scss">
-.theme__list {
+.level__list {
   width: 60vmin;
   padding: 16px;
   font-size: 2.4rem;
